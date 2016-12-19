582cf331ef7b34c5b0bbc235738d25a51b977af8
Marta Sušek <m.susek@live.com>
2016-12-19 12:15:05 +0000

    made it prettier

--- server.js
+++ server.js
@@ -21,19 +21,14 @@ var url = process.env.MONGOLAB_URI || "mongodb://localhost:" + process.env.PORT 
 var id = 1;
 var hostUrl;
 
-    
 //mongoose.connect(process.env.MONGOLAB_URI);
-
 //var shortUrls = mongoose.model({})
 app.get('/', function(req,res){
     res.sendFile(html);
 })
 
 app.get('/url/*', function(req,res){
-    
     hostUrl = req.headers['x-forwarded-proto'] +'://' + req.headers.host;
-    console.log(hostUrl);
-    
     var origUrl = req.url.replace("/url/", "");
     
     if(req.url=='/favicon.ico'){
@@ -41,7 +36,6 @@ app.get('/url/*', function(req,res){
     }
     
     if(validUrl.isUri(origUrl)){
-        console.log('url is valid' + origUrl);
         
         mongo.connect(url, function(err, db){
             if(err){
@@ -70,22 +64,16 @@ app.get('/url/*', function(req,res){
                         var original =output["original-url"];
                         var short = output["short-url"];
                         res.render("response", {original: original, short: short, link: output["short-url"]});
-                        
                     })
                     id++;
-                    console.log(id);
                 }
-           
             })
-         
         }) 
     }
     else{
-        console.log('url is false ' + origUrl);
         res.send("Url is not valid.");
     }
 })      
-  
     
 app.get('/*', function(req,res){
     console.log(req.headers.host + req.url)
@@ -97,17 +85,13 @@ app.get('/*', function(req,res){
         var urls=db.collection('urls');
         urls.findOne({"short-url": hostUrl + req.url}).then(function(found){
             if(found){
-                console.log(found["original-url"])
                 res.redirect(found["original-url"]);
             }
             else
             res.send("Short url was not found");
         })
-        
     })
 })
-
-
 
 app.listen(process.env.PORT || 8080, function(){
     console.log('Listening on port ' + process.env.PORT);
